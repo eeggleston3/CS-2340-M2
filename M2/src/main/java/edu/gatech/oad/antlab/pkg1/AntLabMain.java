8fd87153d780101590f80406e6bfec8e8c96cba4
varmendari14 <varmendari14@gmail.com>
2017-01-24 12:26:31 -0500

    Violet's Second Ant Lab Main Commit

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -77,7 +77,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   toPrint += p5.toString("gburdell5");
                   //Person6 replace P6 with your name
                   //and gburdell5 with your gt id
-                  Person6 p6 = new Person5("P6");
+                  Person6 p6 = new Person6("P6");
                   toPrint += p6.toString("gburdell5");
 		  
         System.out.println(toPrint);
