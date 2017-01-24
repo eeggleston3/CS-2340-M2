661548df1c46ab68d75ebcb359d81299527a29e4
BeamerEA <bwilde7@gatech.edu>
2017-01-24 12:52:59 -0500

    Poorly written version of Person 5 - Brandon Wilde

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person5.java
@@ -4,22 +4,22 @@ package edu.gatech.oad.antlab.person;
  *  A simple class for person 5
  *  returns their name and a
  *  modified string 
- *  
- *  @author Bob
+ *
+ *  @author Brandon Wilde
  *  @version 1.1
  */
 public class Person5 {
-  /** Holds the persons real name */
-  private String name;
-  	/**
+	/** Holds the persons real name */
+	private String name;
+	/**
 	 * The constructor, takes in the persons
 	 * name
 	 * @param pname the person's real name
 	 */
-  public Person5(String pname) {
-    name = pname;
-  }
-  	/**
+	public Person5(String pname) {
+		name = pname;
+	}
+	/**
 	 * This method should take the string
 	 * input and return its characters rotated
 	 * 2 positions.
@@ -30,10 +30,16 @@ public class Person5 {
 	 * @return the modified string
 	 */
 	private String calc(String input) {
-	  //Person 5 put your implementation here
-	  return null;
+		//Person 5 put your implementation here
+		String temp = "";
+		for (int i = 2; i < input.length(); i++) {
+			temp += input[i];
+		}
+		temp += input[0];
+		temp += input[1];
+		return temp;
 	}
-	
+
 	/**
 	 * Return a string rep of this object
 	 * that varies with an input string
@@ -43,7 +49,7 @@ public class Person5 {
 	 *         object
 	 */
 	public String toString(String input) {
-	  return name + calc(input);
+		return name + calc(input);
 	}
 
 }
